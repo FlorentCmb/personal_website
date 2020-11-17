e43fbe8860a56740a75e634c091fca06ed096ed6
FlorentCmb <AS0D@protonmail.ch>
2020-11-17 19:13:21 +0100

    Create SectionContainer component

---
+++ src/components/SectionContainer.js
@@ -0,0 +1,57 @@
+// Librairie
+import React, { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+// Style
+import './SectionContainer.css'
+
+const SectionContainer = props => {
+    // Props
+    const { title, text, img, textToRight } = props
+
+    // States
+    const [isSticky, setIsSticky] = useState(false)
+
+    // When the Navbar is mounted
+    useEffect(() => {
+        // Get element's Y position on the page
+        const element = document.querySelector('.SectionContainer')
+        const height = element.offsetTop
+        // Event listener to decide if the menu is sticky or not
+        window.addEventListener('scroll', () => handleScroll(height))
+
+        // Remove the event listener whenever we destroy the component
+        return () => window.removeEventListener('scroll', () => handleScroll(height))
+    }, [])
+
+    const handleScroll = height => {
+        // +54 is the height of the Navbar so that the component won't take its place
+        window.pageYOffset + 54 > height ? setIsSticky(true) : setIsSticky(false)
+    }
+
+    return (
+        <div className={`SectionContainer ${isSticky ? 'SectionContainer-Sticky' : ''}`} title={title}>
+            <div className="SectionContainer-Text-Container" style={{ order: textToRight ? 2 : 1 }}>
+                <div>
+                    <h3 className="SectionContainer-Title">{title}</h3>
+                    <p className="SectionContainer-Text">{text}</p>
+                </div>
+            </div>
+            <div className="SectionContainer-Image-Container" style={{ order: textToRight ? 1 : 2 }}>
+                <img className="SectionContainer-Image" src={img} alt={title} title={`Source : ${img}`} />
+            </div>
+        </div>
+    )
+}
+
+SectionContainer.propTypes = {
+    title: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    textToRight: PropTypes.bool,
+}
+
+SectionContainer.propTypes = {
+    textToRight: false
+}
+
+export default SectionContainer
