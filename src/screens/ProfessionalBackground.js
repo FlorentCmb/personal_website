79476dc51c3d67748e309e76cfaaf666fee9dffa
FlorentCmb <AS0D@protonmail.ch>
2020-11-18 18:05:02 +0100

    Add dates to ProfessionalBackground

--- src/screens/ProfessionalBackground.js
+++ src/screens/ProfessionalBackground.js
@@ -14,13 +14,13 @@ const ProfessionalBackground = () => {
                 img="https://www.wildcodeschool.com/uploads/0d26f91b3a443423e08192238c82b5ef.jpg-Wild%20Code%20School%20Paris%20-%20Openspace%202.jpg"
             />
             <SectionContainer
-                title="Fullstack Web Developer Course - Wild Code School Paris"
+                title="Fullstack Web Developer Course - Wild Code School Paris (2019 - 2020)"
                 text="Still in the same school, I did this time an intensive training as a fullstack web developer in 5 months, with a 6 months internship at the end. These 5 months were divided in 3 parts, during which we did group projects. We were in groups of 6 or 8, and each project was different. The first project was a static website, using HTML and CSS. The use of Javascript was possible, but not mandatory. In my group, we chose to make the site of a photographer who offered her services in urbex locations. The second project was a project in which we were obliged to use React (the Javascript library). With my group, we chose to make an integrated video game on a dynamic site. Finally, the third project was the only one to have predefined subjects, as they were intended for real customers. Our client wanted an application dedicated to farmers so that they could organize themselves differently and avoid certain accidents. We had to use React but also a server (nodejs) with a relational database (SQL). At the end, there was a big presentation of all the latest projects and a buffet."
                 img="https://cdn.paris.fr/paris/2019/07/24/original-bc41cb6a0f55fc02b98365c568d3f74e.jpeg"
                 textToRight
             />
             <SectionContainer
-                title="6 month internship - 3C Industry"
+                title="6 month internship - 3C Industry (Feb. 2020 - Jul. 2020)"
                 text="As I had to do an internship in order to complete my course at the Wild Code School, I looked for a company that could hire me for 6 months. I applied for an offer from 3C Industry and they quickly answered me. Then I had an interview in which I could see what the company's project was, what programming languages they were using and what my tasks would be. Afterwards, they gave me a technical test to do to see if I had the required skills, which I passed. So I became their intern, and they directly gave me very interesting and diversified tasks. I was also able to learn new technologies, such as Redux, ThreeJS, Bootstrap, MaterialUI or MongoDB. The team was very welcoming, and they almost all worked in different fields. When the health crisis arrived, I was lucky enough to be able to work remotely, doing video conferences to see the team's progress."
                 img="https://media-exp1.licdn.com/dms/image/C561BAQF9HbSLVYv2zw/company-background_10000/0?e=2159024400&v=beta&t=QoSa1TeaMmiirj1h24E9l6sbYIpuaxsV-8ZWHs6_Tbg"
             />
