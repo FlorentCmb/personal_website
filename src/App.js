ef22a4c7c9063ca5c366e645586ac76cea85c809
FlorentCmb <AS0D@protonmail.ch>
2020-11-24 15:04:26 +0100

    Add Home page in App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 // Screens
+import Home from './screens/Home'
 import ProfessionalBackground from './screens/ProfessionalBackground'
 // Components
 import Banner from './components/Banner'
@@ -15,6 +16,9 @@ const App = () => {
       <Banner />
       <Navbar />
       <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
         <Route exact path="/professional-background">
           <ProfessionalBackground />
         </Route>
